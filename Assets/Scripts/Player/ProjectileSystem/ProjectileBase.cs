05e83bd1c285009e01d9d6cc643543ca29eb1dd3
Sydney Kolehmainen <145433757+SydneyPTKT23@users.noreply.github.com>
2023-10-07 21:06:50 +0300

    jotain en muista yeag

--- Assets/Scripts/Player/ProjectileSystem/ProjectileBase.cs
+++ Assets/Scripts/Player/ProjectileSystem/ProjectileBase.cs
@@ -3,24 +3,31 @@ using UnityEngine;
 public class ProjectileBase : MonoBehaviour
 {
     public int damage;
+    public float range;
     public float speed;
 
-    public void Initialize(int t_damage, int t_speed)
+    public void Initialize(int t_damage, int t_range, int t_speed)
     {
         damage = t_damage;
+        range = t_range;
         speed = t_speed;
     }
 
     private void Update()
     {
         transform.position += speed * Time.deltaTime * transform.up;
+        Destroy(gameObject, range);
     }
 
     private void OnTriggerEnter2D(Collider2D t_collider)
     {
-        if (t_collider.TryGetComponent(out Health t_health))
+        if (!t_collider.gameObject.CompareTag("player"))
         {
-            t_health.TakeDamage(damage);
-        }
+            if (t_collider.TryGetComponent(out Health t_health))
+            {
+                t_health.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }        
     }
 }
