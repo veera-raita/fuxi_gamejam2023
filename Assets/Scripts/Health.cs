fef6363d8650327d106207923f3eb9957216fda8
Sydney Kolehmainen <145433757+SydneyPTKT23@users.noreply.github.com>
2023-10-08 14:11:24 +0300

    yeag

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maximumHealth = 50;
+    public GameObject deathEffect;
     public Transform damagePopup;
     public bool invincible = false;
     public int CurrentHealth { get; private set; }
@@ -27,7 +28,6 @@ public class Health : MonoBehaviour
         {
             Transform t_damagePopup = Instantiate(damagePopup, transform.position, Quaternion.identity);
             t_damagePopup.GetComponent<DamagePopup>().Setup(t_damage);
-
             CurrentHealth -= t_damage;
             if (CurrentHealth <= 0)
             {
@@ -38,6 +38,8 @@ public class Health : MonoBehaviour
 
     private void Kill()
     {
+        GameObject t_death = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(t_death, 5f);
         Debug.Log(gameObject.name + " has died!");
         Destroy(gameObject);
     }
--- Assets/Scripts/Player/ProjectileSystem/ProjectileBase.cs
+++ Assets/Scripts/Player/ProjectileSystem/ProjectileBase.cs
@@ -9,11 +9,6 @@ public class ProjectileBase : MonoBehaviour
 
     public GameObject particle;
 
-    private void Start()
-    {
-
-    }
-
     public void Initialize(int t_damage, int t_range, int t_speed)
     {
         damage = t_damage;
@@ -33,8 +28,8 @@ public class ProjectileBase : MonoBehaviour
         {
             if (t_collider.TryGetComponent(out Health t_health))
             {
-                GameObject t_particle = Instantiate(particle, t_collider.transform.position, Quaternion.identity);
-                Destroy(t_particle, 1f);
+                GameObject t_particle = Instantiate(particle, transform.position, Quaternion.identity);
+                Destroy(t_particle, 5f);
                 t_health.TakeDamage(damage);
                 Destroy(gameObject);
             }
