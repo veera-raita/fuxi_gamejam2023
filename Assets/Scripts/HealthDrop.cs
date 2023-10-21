e809765daa284a180bfe973a8163a7c205ae56c6
Veera Raita <fvera003@edu.xamk.fi>
2023-10-21 14:44:52 +0300

    flicker health drops
    :333

--- Assets/Scripts/HealthDrop.cs
+++ Assets/Scripts/HealthDrop.cs
@@ -5,12 +5,44 @@ using UnityEngine;
 public class HealthDrop : MonoBehaviour
 {
     public float timer = 0;
+    public SpriteRenderer HeadSprite;
+
+    private void Start()
+    {
+        StartCoroutine(FlickerJekku());
+    }
 
     private void Update()
     {
+        HeadSprite = GetComponentInChildren<SpriteRenderer>();
+        
         timer += Time.deltaTime;
 
         if (timer >= 15) { Destroy(gameObject); }
+    }
+
+    private IEnumerator FlickerJekku()
+    {
+        yield return new WaitForSeconds(12);
+        HeadSprite.color = new Color(HeadSprite.color.r, HeadSprite.color.g, HeadSprite.color.b, 0.7f);
+        yield return new WaitForSeconds(0.5f);
+        HeadSprite.color = Color.white;
+        yield return new WaitForSeconds(0.5f);
+        HeadSprite.color = new Color(HeadSprite.color.r, HeadSprite.color.g, HeadSprite.color.b, 0.7f);
+        yield return new WaitForSeconds(0.5f);
+        HeadSprite.color = Color.white;
+        yield return new WaitForSeconds(0.5f);
+        HeadSprite.color = new Color(HeadSprite.color.r, HeadSprite.color.g, HeadSprite.color.b, 0.7f);
+        yield return new WaitForSeconds(0.2f);
+        HeadSprite.color = Color.white;
+        yield return new WaitForSeconds(0.2f);
+        HeadSprite.color = new Color(HeadSprite.color.r, HeadSprite.color.g, HeadSprite.color.b, 0.7f);
+        yield return new WaitForSeconds(0.2f);
+        HeadSprite.color = Color.white;
+        yield return new WaitForSeconds(0.2f);
+        HeadSprite.color = new Color(HeadSprite.color.r, HeadSprite.color.g, HeadSprite.color.b, 0.7f);
+        yield return new WaitForSeconds(0.2f);
+        HeadSprite.color = Color.white;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
